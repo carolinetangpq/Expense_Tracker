e6fc965afcb791892b177e025562ec4fd980d0b2
Caroline.Tang <caroline.tang@cognizant.com>
2023-07-26 06:02:47 +0000

    Add new file

---
+++ Record.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackingapp
+{
+    public class Record
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        //public DateTime Date { get; set; }
+
+        public DateTime Date()
+        {
+            return DateTime.Now; 
+        }
+
+
+        public Record(string title, string description, double amount)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Amount = amount;
+        }
+
+        public Record() { }
+
+        public virtual double StoreAmount(double d)
+        {
+            //double ans; 
+            //double ans = double.Parse(Console.ReadLine()); 
+             if (Amount < 0)
+            {
+                Expense e = new Expense();
+                Console.WriteLine("Expense has been added: {0}", d);
+                return e.Amount; 
+            } else
+            {
+                Income i = new Income();
+                //i.Amount = ans;
+                Console.WriteLine("Income has been added: {0}", d);
+                return i.Amount; 
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("\nRecord:\n" +
+                "Title: {0}\nDescription:{1}\nAmount:{2}\nDate:{3}\n", Title, Description, Amount, Date()); 
+        }
+
+    }
+}
