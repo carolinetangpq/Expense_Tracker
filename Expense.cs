83d36cba134a84cb328df2ab16eb54063aa4def7
Caroline.Tang <caroline.tang@cognizant.com>
2023-08-03 09:23:21 +0000

    Update Expense.cs, final code

--- Expense.cs
+++ Expense.cs
@@ -8,18 +8,17 @@ namespace ExpenseTrackingapp
 {
     public class Expense:Record
     {
-        public double ExpenseRecord { get; set; }
-
         public Expense() { }
-        public Expense(double expenseRecord)
+        public override double getAmount()
         {
-            ExpenseRecord = expenseRecord;
+            return base.getAmount();
+ 
         }
 
         public override string ToString()
         {
             return string.Format("Record:\n" +
-                "Title: {0}\nDescription:{1}\nAmount:{2}\nDate:{3}\n", Title, Description, Amount, Date());
+                "Title: {0}\nDescription:{1}\nAmount:{2}\nDate:{3}\n", Title, Description, getAmount(), Date());
         }
 
     }
